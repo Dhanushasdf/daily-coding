f522afa99c0a49378191b939240faa84b26fe828
Dhanush Pagolu <94379238+Dhanushasdf@users.noreply.github.com>
2026-09-19 22:16:10 +0530

    Add files via upload

---
+++ Day-012/firstAndLastOccurrence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	int firstAndLastOccurrence(vector<int>& nums, int target)
+	
+	{
+		int left =0, right = nums.size() - 1;
+		int answer = -1;
+		
+		
+		while (left <= right)
+		{
+			int mid = left + (right - left) / 2;
+			
+			if (nums[mid] == target)
+			{
+				answer = mid;
+				right = mid - 1;
+			}
+			else if (nums[mid] < target)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+			
+		}
+		return answer;	
+	}
+};
+
+int main()
+
+{
+	Solution sol;
+	vector<int> nums = {1, 2, 2, 2, 3, 4, 5};
+	int target = 2;
+	cout << sol.firstAndLastOccurrence(nums, target) << endl;
+}
